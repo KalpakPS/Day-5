1499629ec25df2d127c6b71b8b50cdfc908a3d5a
Kalpak P S <kalpak9446@gmail.com>
2024-07-31 17:08:13 +0530

    ts component given by nest

--- src/app/pages/single/single.component.ts
+++ src/app/pages/single/single.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-single',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './single.component.html',
   styleUrl: './single.component.css'
 })
 export class SingleComponent {
+  constructor(private routerId: ActivatedRoute, private api: ApiService) {}
+  data:any=[]
 
+  ngOnInit() {
+    this.api.getProducts().subscribe((res: any) => {
+      let id = this.routerId.snapshot.paramMap.get('id')
+      let response = res
+      let product = response.filter((e: any) => e.id === id)
+      this.data = product[0]
+      
+    })
+  }
 }
